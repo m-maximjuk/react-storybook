a709de2e4d9554b5100e80c121dcbaaa14a52f15
m-maximjuk <michael.maxim101@gmail.com>
2019-09-18 18:52:53 +0900

    with static storybook

---
+++ storybook-static/main.06f44637c0c8232898cb.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{0:function(n,o,c){c(370),c(478),n.exports=c(765)},1:function(n,o){},478:function(n,o,c){"use strict";c.r(o);c(479),c(762)}},[[0,1,2]]]);
---
+++ storybook-static/precache-manifest.88e674b26c4c13038a1d36911f2f7a3d.js
@@ -0,0 +1,19 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67c2f34e1e5ee7234c4a8f69f3e92447",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.b094e815e6729671a278.bundle.js"
+  },
+  {
+    "revision": "2460d749f27679dfebc8",
+    "url": "main.css"
+  },
+  {
+    "url": "runtime~main.b094e815e6729671a278.bundle.js"
+  },
+  {
+    "url": "vendors~main.b094e815e6729671a278.bundle.js"
+  }
+]);
