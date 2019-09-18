5d24af15b4effdf30155f28dddf2af81a021d0e0
m-maximjuk <michael.maxim101@gmail.com>
2019-09-18 16:07:09 +0900

    snapshot

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,7 @@
 import { configure } from '@storybook/react';
+import requireContext from 'require-context.macro'
 import '../src/index.css'
 
 // automatically import all files ending in *.stories.js
 // configure(require.context('../stories', true, /\.stories\.js$/), module);
-configure(require.context('../src', true, /\.stories\.js$/), module);
+configure(requireContext('../src', true, /\.stories\.js$/), module);
