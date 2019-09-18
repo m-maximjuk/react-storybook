3dd7c9b9b6695376ba236ce0485908dd220d1f63
m-maximjuk <michael.maxim101@gmail.com>
2019-09-18 18:43:44 +0900

    with chromatic

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,6 @@
 import { configure } from '@storybook/react';
 import requireContext from 'require-context.macro'
+import 'storybook-chromatic'
 import '../src/index.css'
 
 // automatically import all files ending in *.stories.js
